915588df2c89971b741a4077addac517bf472f4f
Fernando Longuini <flonguini@outlook.com>
2018-07-08 20:38:08 -0300

    SQLite - Insert

--- DesktopContactsApp/NewContactWindow.xaml.cs
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,6 +29,21 @@ namespace DesktopContactsApp
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             //TODO: Save contact
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phonenumber = phoneNumberTextBox.Text
+            };
+
+            string databaseName = "Contacts.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string databasePath = System.IO.Path.Combine(folderPath,databaseName);
+
+            SQLiteConnection connection = new SQLiteConnection(databasePath);
+            connection.CreateTable<Contact>(); //Cria a tabela de contatos
+            connection.Insert(contact);
+            connection.Close();
             this.Close();
         }
     }
