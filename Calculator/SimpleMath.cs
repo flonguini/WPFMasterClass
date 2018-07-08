dc428bba79dba7aca65c09a1b9083a9959bcb2b4
Fernando Longuini <flonguini@outlook.com>
2018-07-08 00:43:53 -0300

    Mensagem divisao por zero

--- Calculator/SimpleMath.cs
+++ Calculator/SimpleMath.cs
@@ -1,4 +1,6 @@
-﻿namespace Calculator
+﻿using System.Windows;
+
+namespace Calculator
 {
     public class SimpleMath
     {
@@ -16,6 +18,10 @@
         }
         public static double Divide(double number1, double number2)
         {
+            if (number2 == 0)
+            {
+                MessageBox.Show("Division by 0 is not suported", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
             return number1 / number2;
         }
     }
