184546d13827e6a89f094daa5df413f3ee8ecaaa
Fernando Longuini <flonguini@outlook.com>
2018-07-08 23:01:24 -0300

    Listview binding implemented

--- DesktopContactsApp/Classes/Contact.cs
+++ DesktopContactsApp/Classes/Contact.cs
@@ -14,5 +14,10 @@ namespace DesktopContactsApp.Classes
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phonenumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Phonenumber}";
+        }
     }
 }
--- DesktopContactsApp/MainWindow.xaml.cs
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
+﻿using DesktopContactsApp.Classes;
+using SQLite;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DesktopContactsApp
 {
@@ -23,6 +15,7 @@ namespace DesktopContactsApp
         public MainWindow()
         {
             InitializeComponent();
+            ReadDatabase();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -30,6 +23,30 @@ namespace DesktopContactsApp
             NewContactWindow newContact = new NewContactWindow();
             //newContact.Show();
             newContact.ShowDialog();
+
+            ReadDatabase();
+        }
+
+        void ReadDatabase()
+        {
+            List<Contact> contacts;
+            using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                contacts = conn.Table<Contact>().ToList();
+            }
+
+            if (contacts != null)
+            {
+                //foreach (var c in contacts)
+                //{
+                //    ContactsListView.Items.Add(new ListViewItem()
+                //    {
+                //        Content = c
+                //    });
+                //}
+                ContactsListView.ItemsSource = contacts;
+            }
         }
     }
 }
