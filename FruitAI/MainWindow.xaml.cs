751bfd2f1fc4c40b14adb9a46f6f2e615edd6dd0
Fernando Longuini <flonguini@outlook.com>
2018-07-09 03:45:28 -0300

    FruitAi Http request created

---
+++ FruitAI/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace FruitAI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Filter = "Image Files (*.png, *.jpg)|*.png;*.jpg;*.jpeg|All Files (*.*)|*.*"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                string fileName = dialog.FileName;
+                SelectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePredictionAsync(fileName);
+            }
+        }
+
+        private async void MakePredictionAsync(string fileName)
+        {
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/57f88bd9-e839-478b-9ce5-46799a762640/image?iterationId=b82bc964-be74-4d3d-a698-58929d57f375";
+            string prediction_key = "8860944f38e54f4d957a0d33583f0574";
+            string content_type = "application/octet-stream";
+            var file = File.ReadAllBytes(fileName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", prediction_key);
+
+                using (var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(content_type);
+                    var response = await client.PostAsync(url, content);
+                }
+            }
+
+        }
+    }
+}
