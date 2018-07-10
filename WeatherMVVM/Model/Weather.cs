35c1f3068fe67c0e5361af4ae739137d3e7a46ca
Fernando Longuini <flonguini@outlook.com>
2018-07-10 00:16:50 -0300

    Model and ViewModel created

---
+++ WeatherMVVM/Model/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherMVVM.Model
+{
+    public class Data
+    {
+        public double Temperature { get; set; }
+        public string WindDirection { get; set; }
+        public int WindVelocity { get; set; }
+        public int Humidity { get; set; }
+        public string Condition { get; set; }
+        public int Pressure { get; set; }
+        public string Icon { get; set; }
+        public int Sensation { get; set; }
+        public string Date { get; set; }
+    }
+
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public Data Data { get; set; }
+    }
+
+}
---
+++ WeatherMVVM/WeatherViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherMVVM.Model;
+
+namespace WeatherMVVM
+{
+    public class WeatherViewModel
+    {
+        public const string API_KEY = "42e2e4db0db214874223d3d73da630e3";
+        public const string BASE_URL = "http://apiadvisor.climatempo.com.br/api/v1/weather/locale/3477/current?token={0}&id={1}";
+
+        public async Weather GetWeatherInformation(string cityName)
+        {
+            var result = new Weather();
+
+            string url = string.Format(BASE_URL, API_KEY, cityName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+            }
+
+            return result;
+        }
+    }
+}
